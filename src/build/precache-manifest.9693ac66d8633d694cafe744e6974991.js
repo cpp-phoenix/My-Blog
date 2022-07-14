2fd6aa315b20a4c642a173e97d0c7acc42ba1850
Phoenix <phoenix@Phoenixs-MacBook-Air.local>
2022-07-15 00:54:56 +0530

    complete blog API development wrapup

---
+++ src/build/precache-manifest.9693ac66d8633d694cafe744e6974991.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a6ed86d8846bc15503a",
+    "url": "/static/css/main.595b9bbd.chunk.css"
+  },
+  {
+    "revision": "5a6ed86d8846bc15503a",
+    "url": "/static/js/main.7da98cdc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f7125d3c337e9d104bda",
+    "url": "/static/js/2.f8b8cd58.chunk.js"
+  },
+  {
+    "revision": "4972a80c58c12822e02ba71b59c3aff1",
+    "url": "/index.html"
+  }
+];
